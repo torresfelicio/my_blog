26d0177e9addba3e17233c034b88dcdc422886a7
Lucas Torres <lucastorresfellicio@gmail.com>
2021-02-23 20:51:12 -0300

    feat: criado title do blog

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -15,6 +15,7 @@ class MyDocument extends Document {
             href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;700&display=swap"
             rel="stylesheet"
           />
+          <title>Lucas Torres</title>
         </Head>
         <body>
           <Main />
